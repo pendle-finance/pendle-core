a09e1ff59a65e25c6522e56c4ea3e0fa4dad52a6
UncleGrandpa925 <vuonglongtoan@gmail.com>
2021-02-13 02:45:25 +0800

    new math library

---
+++ contracts/libraries/PendleMathV2.sol
@@ -0,0 +1,241 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+/*
+ * GNU General Public License v3.0 or later
+ * ========================================
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+pragma solidity ^0.7.0;
+pragma experimental ABIEncoderV2;
+
+import "@openzeppelin/contracts/math/SafeMath.sol";
+
+library Math {
+    using SafeMath for uint256;
+
+    uint256 internal constant UINT_MAX_VALUE = uint256(-1);
+    uint256 internal constant WAD = 1e18;
+    uint256 internal constant BIG_NUMBER = (uint256(1) << uint256(200));
+    uint256 internal constant PRECISION_BITS = 40;
+    uint256 internal constant RONE = uint256(1) << PRECISION_BITS;
+    uint256 internal constant PI = (314 * RONE) / 10**2;
+    uint256 internal constant PI_PLUSONE = (414 * RONE) / 10**2;
+    uint256 internal constant PRECISION_POW = 1e2;
+
+    function checkMultOverflow(uint256 _x, uint256 _y) internal pure returns (bool) {
+        if (_y == 0) return false;
+        return (((_x * _y) / _y) != _x);
+    }
+
+    /**
+    @notice find the integer part of log2(p/q)
+        => find largest x s.t p >= q * 2^x
+        => find largest x s.t 2^x <= p / q
+     */
+    function log2Int(uint256 _p, uint256 _q) internal pure returns (uint256) {
+        uint256 res = 0;
+        uint256 remain = _p / _q;
+        while (remain > 0) {
+            res++;
+            remain /= 2;
+        }
+        return res - 1;
+    }
+
+    /**
+    @notice log2 for a number that it in [1,2)
+    @dev function is from Kyber.
+     */
+    function log2ForSmallNumber(uint256 _x) internal pure returns (uint256) {
+        uint256 res = 0;
+        uint256 one = (uint256(1) << PRECISION_BITS);
+        uint256 two = 2 * one;
+        uint256 addition = one;
+
+        require((_x >= one) && (_x <= two));
+        require(PRECISION_BITS < 125);
+
+        for (uint256 i = PRECISION_BITS; i > 0; i--) {
+            _x = (_x * _x) / one;
+            addition = addition / 2;
+            if (_x >= two) {
+                _x = _x / 2;
+                res += addition;
+            }
+        }
+
+        return res;
+    }
+
+    /**
+    @notice log2 of (p/q). returns result in FP form
+    @dev function is from Kyber. Long have verified this function
+            to be working correctly
+     */
+    function logBase2(uint256 _p, uint256 _q) internal pure returns (uint256) {
+        uint256 n = 0;
+
+        if (_p > _q) {
+            n = log2Int(_p, _q);
+        }
+
+        require(!checkMultOverflow(_p, RONE));
+        require(!checkMultOverflow(n, RONE));
+        require(!checkMultOverflow(uint256(1) << n, _q));
+
+        uint256 y = (_p * RONE) / (_q * (uint256(1) << n));
+        uint256 log2Small = log2ForSmallNumber(y);
+
+        require(n * RONE <= BIG_NUMBER);
+        require(log2Small <= BIG_NUMBER);
+
+        return n * RONE + log2Small;
+    }
+
+    /**
+    @notice calculate ln(p/q). returned result >= 0
+    @dev function is from Kyber. Long have verified this function
+            to be working correctly
+    */
+    function ln(uint256 p, uint256 q) internal pure returns (uint256) {
+        uint256 ln2Numerator = 6931471805599453094172;
+        uint256 ln2Denomerator = 10000000000000000000000;
+
+        uint256 log2x = logBase2(p, q);
+
+        require(!checkMultOverflow(ln2Numerator, log2x));
+
+        return (ln2Numerator * log2x) / ln2Denomerator;
+    }
+
+    /**
+    @notice extract the fractional part of an FP
+     */
+    function fpart(uint256 value) internal pure returns (uint256) {
+        return value % RONE;
+    }
+
+    /**
+    @notice convert an FP to an Int
+     */
+    function toInt(uint256 value) internal pure returns (uint256) {
+        return value / RONE;
+    }
+
+    /**
+    @notice convert an Int to an FP
+     */
+    function toFP(uint256 value) internal pure returns (uint256) {
+        return value * RONE;
+    }
+
+    /**
+    @notice return e^exp in FP form
+    @dev estimation by formula at http://pages.mtu.edu/~shene/COURSES/cs201/NOTES/chap04/exp.html
+        the function is based on exp function of:
+        https://github.com/NovakDistributed/macroverse/blob/master/contracts/RealMath.sol
+    @dev the function is expected to converge quite fast, after about 20 iteration
+
+     */
+    function rpowe(uint256 exp) internal pure returns (uint256) {
+        uint256 res = 0;
+
+        uint256 curTerm = RONE;
+
+        /* no limit on n. If the function cannot converge, it will lead to out of gas*/
+        for (uint256 n = 0; ; n++) {
+            res += curTerm;
+            curTerm = rmul(curTerm, rdiv(exp, toFP(n + 1)));
+            if (curTerm == 0) {
+                break;
+            }
+        }
+
+        return res;
+    }
+
+    /**
+    @notice calculate base^exp with base and exp being FP int
+    @dev to improve accuracy, base^exp = base^(int(exp)+frac(exp))
+                                       = base^int(exp) * base^frac
+     */
+    function rpow(uint256 base, uint256 exp) internal pure returns (uint256) {
+        if (exp == 0) {
+            // Anything to the 0 is 1
+            return RONE;
+        }
+
+        uint256 frac = fpart(exp); // get the fractional part
+        uint256 whole = exp - frac;
+
+        uint256 wholePow = rpowi(base, toInt(whole)); // whole is an FP, convert to Int
+        uint256 fracPow;
+
+        // instead of calculating base ^ frac, we will calculate e ^ (frac*ln(base))
+        if (base < RONE) {
+            /* since the base is smaller than 1.0, ln(base) < 0.
+            Since 1 / (e^(frac*ln(1/base))) = e ^ (frac*ln(base)),
+            we will calculate 1 / (e^(frac*ln(1/base))) instead.
+            */
+            uint256 newExp = rmul(frac, ln(rdiv(RONE, base), RONE));
+            fracPow = rdiv(RONE, rpowe(newExp));
+        } else {
+            /* base is greater than 1, calculate normally */
+            uint256 newExp = rmul(frac, ln(base, RONE));
+            fracPow = rpowe(newExp);
+        }
+        return rmul(wholePow, fracPow);
+    }
+
+    /**
+    @notice return a^q with a in FP form and q in Int
+    @dev this function use a technique called: exponentiating by squaring
+        complexity O(log(q))
+    @dev (q & 1 > 0) is a faster way to check if q % 2 !=0 or not
+        q>>=1 is a faster way to calculate q/=2
+     */
+    function rpowi(uint256 a, uint256 q) internal pure returns (uint256) {
+        uint256 res = RONE;
+        while (q > 0) {
+            if ((q & 1) > 0) res = rmul(res, a);
+            a = rmul(a, a);
+            q >>= 1;
+        }
+        return res;
+    }
+
+    /**
+    @notice divide 2 FP, return an FP
+    @dev function is from Balancer.
+     */
+    function rdiv(uint256 x, uint256 y) internal pure returns (uint256) {
+        return (y / 2).add(x.mul(RONE)).div(y);
+    }
+
+    /**
+    @notice multiply 2 FP, return an FP
+    @dev function is from Balancer.
+     */
+    function rmul(uint256 x, uint256 y) internal pure returns (uint256) {
+        return (RONE / 2).add(x.mul(y)).div(RONE);
+    }
+
+    function max(uint256 a, uint256 b) internal pure returns (uint256) {
+        return a >= b ? a : b;
+    }
+
+    function min(uint256 a, uint256 b) internal pure returns (uint256) {
+        return a < b ? a : b;
+    }
+}
