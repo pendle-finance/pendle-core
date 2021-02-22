313cc15ee6f7f8f9f22e6dc0b9e5c7da35943d86
UncleGrandpa925 <vuonglongtoan@gmail.com>
2021-02-22 19:41:06 +0800

    Add tests for rpow of mathLib

--- test/core/pendleMockMathLibrary.spec.ts
+++ test/core/pendleMockMathLibrary.spec.ts
@@ -1,17 +1,47 @@
-import { BigNumber as BN } from "ethers";
+import { BigNumber as BN, Contract } from "ethers";
 import MockMathLibrary from "../../build/artifacts/contracts/mock/MockMathLibrary.sol/MockMathLibrary.json";
-import Math from '../../build/artifacts/contracts/libraries/MathLib.sol/Math.json';
+import { evm_revert, evm_snapshot } from "../helpers";
+import { expect } from "chai";
+import testData from "./fixtures/pendleMockMathLibrary.json";
 
 const { waffle } = require("hardhat");
-const { provider, deployContract, link } = waffle;
+const { provider, deployContract } = waffle;
 
-it("test 1", async () => {
+describe("Math tests", async () => {
   const wallets = provider.getWallets();
   const [alice, bob] = wallets;
+  let math: Contract;
+  let globalSnapshotId: string;
+  let rpowTest: string[][];
+  before(async () => {
+    globalSnapshotId = await evm_snapshot();
+    math = await deployContract(alice, MockMathLibrary, []);
+    rpowTest = (<any>testData).test;
+  });
 
-  let myLibrary = await deployContract(alice, Math, []);
-  console.log(await myLibrary.rpow(BN.from(2197099110203), BN.from(549755813888)));
-  // await mock.rpow(BN.from(2826053734965), BN.from(549755813888));
-  // await mock.rpow(BN.from(2197099110203), BN.from(549755813888));
-  // await mock.rpowi(BN.from(2197099110203), BN.from(0));
+  after(async () => {
+    await evm_revert(globalSnapshotId);
+  });
+
+  beforeEach(async () => {
+    // await evm_revert(snapshotId);
+    // snapshotId = await evm_snapshot();
+  });
+
+  it("test rpow", async () => {
+    const toleranceRatio = BN.from(10).pow(8);
+    for (let id = 0; id < rpowTest.length; id++) {
+      let [base, exp, _expectedResult] = rpowTest[id];
+      let expectedResult: BN = BN.from(_expectedResult);
+      let actualResult: BN = await math.rpow(BN.from(base), BN.from(exp));
+      let diff = expectedResult.sub(actualResult);
+      if (diff.lte(0)) diff = diff.mul(-1);
+      // console.log(base, exp, diff.toString(), actualResult.toString(), expectedResult.toString());
+      // if diff < 10, then the difference is minimal
+      if (diff.gte(10)) {
+        // diff * toleranceRatio <= expectedResult
+        expect(diff.mul(toleranceRatio).lte(expectedResult), "high precision error").to.be.true;
+      }
+    }
+  });
 });
