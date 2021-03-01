b40dbc74da08fdad67b2ffe93016e13cb872c6db
UncleGrandpa925 <vuonglongtoan@gmail.com>
2021-03-01 23:12:36 +0800

    Delete redundant typechain in solcover.js

--- .solcover.js
+++ .solcover.js
@@ -2,9 +2,6 @@ const shell = require('shelljs');
 
 module.exports = {
   skipFiles: ['core/PendleTreasury.sol', 'core/PendleGovernance.sol', 'periphery/Timelock.sol', 'tokens/WETH9.sol', 'tokens/PENDLE.sol'],
-  onCompileComplete: async function (_config) {
-    await run("typechain");
-  },
   // We need to do this because solcover generates bespoke artifacts.
   onIstanbulComplete: async function (_config) {
     shell.rm("-rf", "./artifacts"); // Or your config.paths.artifacts path
