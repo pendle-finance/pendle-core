2b3ea875003a2851648f2c541d1199b25705352b
UncleGrandpa925 <vuonglongtoan@gmail.com>
2021-03-19 18:07:24 +0800

    Some changes to list of exclude files from coverage

--- .solcover.js
+++ .solcover.js
@@ -2,7 +2,7 @@ const shell = require('shelljs');
 
 module.exports = {
   skipFiles: ['core/PendleTreasury.sol', 'core/PendleGovernance.sol', 'periphery/Timelock.sol',
-    'tokens/WETH9.sol', 'tokens/PENDLE.sol', 'WadRayMath.sol', 'AaveErrors.sol', 'mock/', 'periphery/'],
+    'tokens/WETH9.sol', 'libraries/WadRayMath.sol', 'AaveErrors.sol', 'mock/', 'periphery/'],
   mocha: {
     grep: "@skip-on-coverage", // Find everything with this tag
     invert: true,              // Run the grep's inverse set.
