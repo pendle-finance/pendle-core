13f0b696bfeba9ce4320a89bc227a28ee4163e09
vicom <vunguyenvyvp@gmail.com>
2021-05-12 00:34:06 +0700

    Add tests for PendleRedeemProxy

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -70,6 +70,7 @@ const config: HardhatUserConfig = {
       url: 'http://127.0.0.1:8545',
       gas: 12400000,
       timeout: 1000000,
+      gasPrice: 1
     },
     kovan: {
       url: `https://kovan.infura.io/v3/${process.env.INFURA_KEY || "dummyKey"}`,
