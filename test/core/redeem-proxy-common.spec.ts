48b0b5894ee398e7c1d78eaf8c4d7ec8ba7fc4ad
vicom <vunguyenvyvp@gmail.com>
2021-05-19 16:11:11 +0700

    Add script for querying a deployed instance of contracts

--- test/core/redeem-proxy-common.spec.ts
+++ test/core/redeem-proxy-common.spec.ts
@@ -21,6 +21,7 @@ import { liqParams, liquidityMiningFixture, UserStakeAction } from "./fixtures";
 import * as scenario from "./fixtures/liquidityMiningScenario.fixture";
 
 const { waffle } = require("hardhat");
+const hre = require("hardhat");
 const { provider, deployContract } = waffle;
 
 describe("Redeem Proxy", async () => {
@@ -75,15 +76,41 @@ describe("Redeem Proxy", async () => {
   it("should be able to redeem using redeemProxy", async () => {
     await setTimeNextBlock(provider, params.START_TIME.add(100));
     const aliceBalance = await market.balanceOf(alice.address);
+    console.log(`alice LP Balance  = ${aliceBalance}`);
+
+    await setTimeNextBlock(provider, params.START_TIME.add(consts.ONE_MONTH));
+
+
     await liq.stake(EXPIRY, aliceBalance.div(2), consts.HIGH_GAS_OVERRIDE);
-    const results = await redeemProxy.callStatic.redeem(
+    await setTimeNextBlock(provider, params.START_TIME.add(consts.THREE_MONTH));
+    await alice.sendTransaction({to: bob.address, value: 1});
+
+    console.log(`alice pending lpInterest = ${await router.callStatic.redeemLpInterests(market.address, alice.address)}`);
+    console.log(`alice xyt balance  = ${await xyt.balanceOf(alice.address)}`);
+    console.log(`alice pending xyt interest  = ${await router.callStatic.redeemDueInterests(
+      consts.FORGE_AAVE,
+      tokens.USDT.address,
+      EXPIRY,
+      alice.address
+    )}`);
+
+    // print out results
+    const redeemProxyWeb3 = new hre.web3.eth.Contract(
+      PendleRedeemProxy.abi,
+      redeemProxy.address
+    );
+    const results = await redeemProxyWeb3.methods.redeem(
       [xyt.address, xyt2.address],
       [market.address],
       [liq.address, liq.address],
       [EXPIRY, EXPIRY],
       1
-    );
+    ).call();
     console.log(JSON.stringify(results, null, "  "));
+
+
+
+    console.log(`aUSDT before = ${await aUSDT.balanceOf(alice.address)}`);
     await redeemProxy.redeem(
       [xyt.address, xyt2.address],
       [market.address],
@@ -92,5 +119,6 @@ describe("Redeem Proxy", async () => {
       1,
       consts.HIGH_GAS_OVERRIDE
     );
+    console.log(`aUSDT after = ${await aUSDT.balanceOf(alice.address)}`);
   });
 });
